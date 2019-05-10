4ea98a157646752eb47727490b2e109eb9329b39
Jose Luis Gonzalez <puchosoft@gmail.com>
2019-05-10 12:33:55 -0300

    Finalizamos Tarea 3 - Punto 3

--- js/estadisticas.js
+++ js/estadisticas.js
@@ -3,24 +3,24 @@ var statistics = {
     {
       "Party" : "Republican",
       "Number_of_Reps": 0,
-      "%_Voted_with_Party" : 0
+      "votes_with_party_pct" : 0
     },
     {
       "Party" : "Democrat",
       "Number_of_Reps": 0,
-      "%_Voted_with_Party" : 0
+      "votes_with_party_pct" : 0
     },
     {
       "Party" : "Independent",
       "Number_of_Reps": 0,
-      "%_Voted_with_Party" : 0
+      "votes_with_party_pct" : 0
     },
   ],
   
   "Total" :{
       "Party" : "Total",
       "Number_of_Reps": 0,
-      "%_Voted_with_Party" : 0
+      "votes_with_party_pct" : 0
     },
         
 /*  "Least_Engaged" : [
@@ -55,20 +55,33 @@ var statistics = {
     }
   ] */
 };
+function getMiembrosByParty(party){
+	return(miembros.filter(miembro => miembro.party == party));
+}
+
+function getPorcentAvgOfParty(party){
+	var promedio = (party.length>0?(party.reduce((suma,miembro)=>(suma + miembro.votes_with_party_pct),0)/party.length).toFixed(2):'-');
+	return(promedio);
+}
 
 // Crea un array con los miembros de la camara
 var miembros = data.results[0].members;
 
-var republicans = miembros.filter(miembro => miembro.party == "R");
-var democrats = miembros.filter(miembro => miembro.party == "D");
-var independents = miembros.filter(miembro => miembro.party == "I");
+var republicans = getMiembrosByParty("R");
+var democrats = getMiembrosByParty("D");
+var independents = getMiembrosByParty("I");
 
 statistics.Senate_at_a_glance[0].Number_of_Reps = republicans.length;
 statistics.Senate_at_a_glance[1].Number_of_Reps = democrats.length;
 statistics.Senate_at_a_glance[2].Number_of_Reps = independents.length;
 
+statistics.Senate_at_a_glance[0].votes_with_party_pct = getPorcentAvgOfParty(republicans);
+statistics.Senate_at_a_glance[1].votes_with_party_pct = getPorcentAvgOfParty(democrats);
+statistics.Senate_at_a_glance[2].votes_with_party_pct = getPorcentAvgOfParty(independents);
+
 statistics.Total.Number_of_Reps = statistics.Senate_at_a_glance.reduce((suma,party)=>(suma + party.Number_of_Reps),0);
 
-
+// Totalizamos los porcentajes de cada partido en forma ponderada
+statistics.Total.votes_with_party_pct = (statistics.Senate_at_a_glance.reduce((suma,party)=>(suma + party.votes_with_party_pct * party.Number_of_Reps / statistics.Total.Number_of_Reps),0)).toFixed(2);
 
 console.log(JSON.stringify(statistics));
