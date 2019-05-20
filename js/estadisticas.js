b26768dc5dc32c0b8d98fcdbeff9a7ba1c043fd1
Jose Luis Gonzalez <puchosoft@gmail.com>
2019-05-20 06:51:19 -0300

    Task 3 - Punto 6 - Finalizado 20/05/2019

--- js/estadisticas.js
+++ js/estadisticas.js
@@ -17,16 +17,10 @@ var statistics = {
     },
   ],
 
-  "total" :{
-      "party" : "Total",
-      "number_of_reps": 0,
-      "votes_with_party_pct" : 0
-    },
-
   "least_engaged" : [],
 
   "most_engaged" : [],
-  
+
   "least_loyal" : [],
 
   "most_loyal" : []
@@ -91,6 +85,47 @@ function storeStatistics(key, array){
   );
 }
 
+// Devuelve un <thead> formateado segun 'head'
+function getTHead(head){
+  var thead = document.createElement("thead");
+  var tr = document.createElement("tr");
+  head.forEach(
+    function(h){
+      var th = document.createElement("th");
+      th.innerText = h;
+      tr.appendChild(th);
+    }
+  );
+  thead.appendChild(tr);
+  return(thead);
+}
+
+// Devuelve un <tbody> que incluye los datos del objeto 'id'
+function getTBody(id){
+  var tbody = document.createElement("tbody");
+  statistics[id].forEach(
+    function(o){
+      var tr = document.createElement("tr");
+      var keys = Object.keys(o);
+      keys.forEach(
+        function(k){
+          var td = document.createElement("td");
+          td.innerText = o[k];
+          tr.appendChild(td);
+        }
+      );
+      tbody.appendChild(tr);
+    }
+  );
+  return(tbody);
+}
+
+function drawTable(id, head){
+  var table = document.getElementById(id);
+  table.appendChild(getTHead(head));
+  table.appendChild(getTBody(id));
+}
+
 // Crea un array con los miembros de la camara
 var miembros = data.results[0].members;
 
@@ -107,39 +142,56 @@ statistics.at_a_glance[0].votes_with_party_pct = getPorcentAvgOfParty(republican
 statistics.at_a_glance[1].votes_with_party_pct = getPorcentAvgOfParty(democrats);
 statistics.at_a_glance[2].votes_with_party_pct = getPorcentAvgOfParty(independents);
 
-statistics.total.number_of_reps = statistics.at_a_glance.reduce((suma,party)=>(suma + party.number_of_reps),0);
+// Totaliza 'number_of_reps'
+var total_number_of_reps = statistics.at_a_glance.reduce((suma,party)=>(suma + party.number_of_reps),0);
 
-// Totaliza los porcentajes "De un vistazo" en forma ponderada
-statistics.total.votes_with_party_pct = (statistics.at_a_glance.reduce((suma,party)=>(suma + party.votes_with_party_pct * party.number_of_reps / statistics.total.number_of_reps),0)).toFixed(2);
+// Promedia 'votes_with_party_pct' en forma ponderada
+var total_votes_with_party_pct = (statistics.at_a_glance.reduce((suma,party)=>(suma + party.votes_with_party_pct * party.number_of_reps / total_number_of_reps),0)).toFixed(2);
 
+//Almacena los totales
+statistics.at_a_glance.push({
+  'party' : 'Total',
+  'number_of_reps': total_number_of_reps,
+  'votes_with_party_pct' : total_votes_with_party_pct
+});
+
+drawTable('at_a_glance',['Party','Nº of Reps','% Voted w/ Party']);
+
+// Calcula las estadisticas 'Loyalty'
 var loyalty = document.getElementById("loyalty");
 if(loyalty != null){
+  var head = ['Name', 'Nº Party Votes', '% Party Votes'];
   /*  Obtiene un array del 10% de miembros MENOS LEALES,
       lo ordena de menor a mayor y almacena lo importante en 'statistics' */
   var bottom_10_pct_members = getMembersByPct(miembros, 'votes_with_party_pct', 10, true);
   orderMembersByKey(bottom_10_pct_members, 'votes_with_party_pct', true);
   storeStatistics('least_loyal', bottom_10_pct_members);
+  drawTable('least_loyal',head);
 
   /*  Obtiene un array del 10% de miembros MAS LEALES,
       lo ordena de mayor a menor  y almacena lo importante en 'statistics' */
   var top_10_pct_members = getMembersByPct(miembros, 'votes_with_party_pct', 10, false);
   orderMembersByKey(top_10_pct_members,'votes_with_party_pct', false);
   storeStatistics('most_loyal', top_10_pct_members);
+  drawTable('most_loyal',head);
 }
 
-var engaged = document.getElementById("engaged");
+// Calcula las estadisticas 'Engaged'
+var engaged = document.getElementById('engaged');
 if(engaged != null){
+  var head = ['Name', 'Nº Missed Votes', '% Missed'];
   /*  Obtiene un array del 10% de miembros MENOS COMPROMETIDOS,
       lo ordena de mayor a menor y almacena lo importante en 'statistics' */
   var bottom_10_pct_members = getMembersByPct(miembros, 'missed_votes_pct', 10, false);
   orderMembersByKey(bottom_10_pct_members, 'missed_votes_pct', false);
   storeStatistics('least_engaged', bottom_10_pct_members);
+  drawTable('least_engaged', head);
 
   /*  Obtiene un array del 10% de miembros MAS COMPROMETIDOS,
       lo ordena de menor a mayor  y almacena lo importante en 'statistics' */
   var top_10_pct_members = getMembersByPct(miembros, 'missed_votes_pct', 10, true);
   orderMembersByKey(top_10_pct_members,'missed_votes_pct', true);
   storeStatistics('most_engaged', top_10_pct_members);
+  drawTable('most_engaged', head);
 }
 
-console.log(JSON.stringify(statistics));
